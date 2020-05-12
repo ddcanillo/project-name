98ffadace6a3f91524c97747f8e747bbafa7ee8a
Doms <canillo.dominic@gmail.com>
2020-05-12 16:31:38 +0800

    added eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import crypto from "crypto";
-const myRandom = () => {
-  return crypto.randomBytes(16).toString("hex");
-};
+import crypto from 'crypto';
 
+const myRandom = () => {
+  return crypto.randomBytes(16).toString('hex');
+};
 export default myRandom;
